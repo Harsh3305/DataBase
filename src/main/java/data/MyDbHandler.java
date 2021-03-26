78e82f608d577461f5c6cb8d9c44f026e7a89b1b
Harsh Verma <Harsh>
2021-03-26 21:18:24 +0530

    Initial Commit

---
+++ src/main/java/data/MyDbHandler.java
@@ -0,0 +1,42 @@
+package data;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import Model.Contact;
+import params.Params;
+
+public class MyDbHandler extends SQLiteOpenHelper {
+
+    public MyDbHandler(Context context) {
+        super(context, Params.DB_NAME, null, Params.DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String create = "CREATE TABLE " + Params.DB_NAME + "(" +
+                Params.KEY_ID + " INTEGER PRIMARY KEY,"+ Params.KEY_NAME +
+                 "Text, "+ Params.KEY_PHONE + " TEXT" + ")";
+        Log.d("Database log","Following query is running: " + create);
+        db.execSQL(create);
+    }
+    public void addContact (Contact contact) {
+        SQLiteDatabase database = this.getReadableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Params.KEY_NAME, contact.getName());
+        values.put(Params.KEY_PHONE, contact.getPhoneNumber());
+
+        database.insert(Params.TABLE_NAME, null, values);
+        Log.d("Database log","Data added to DB");
+        database.close();
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ src/main/java/params/Params.java
@@ -0,0 +1,14 @@
+package params;
+
+public class Params {
+
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "contracts_dp";
+    public static final String TABLE_NAME = "contracts_name";
+
+    //Keys of our table in db
+    public static final String KEY_ID = "ID";
+    public static final String KEY_NAME = "ID";
+    public static final String KEY_PHONE = "ID";
+
+}
